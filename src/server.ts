088f354f8756a096c70dd705fb9667061a6e7820
Anderson Marlon <anderson18.marlon@gmail.com>
2024-03-19 00:20:52 -0300

    Added: Routes - get/links & post/links

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,59 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { sql } from './lib/postgres'
+import postgres from 'postgres'
 
 const app = fastify()
+const portNumber = 3000
 
 app.get('/', () => {
-    return 'Hello World!'
+    
+})
+
+app.get('/links', async () => {
+    const result = await sql/*sql*/`
+        SELECT *
+        FROM shortlinks
+        ORDER BY created_at DESC
+    `
+
+    return result
+})
+
+app.post('/links', async (req, reply) => {
+    const createLinkSchema = z.object({
+        code: z.string().min(3),
+        url: z.string().url(),
+    }).parse(req.body)
+
+    // FIXME: Property 'parse' does not exist on type '{ url: string; code: string; }'.
+    const { code, url } = createLinkSchema.parse(req.body)
+
+    try {
+        const result = await sql/*sql*/`
+        INSERT INTO shortlinks (code, original_url)
+        VALUES (${code}, ${url})
+        RETURNING id
+    `
+
+    const link = result[0]
+
+    return reply.status(201).send({ shortLinkId: link.id})
+    } catch (error) {
+        if (error instanceof postgres.PostgresError) {
+            if (error.code === '23505') {
+                return reply.status(400).send({ message: 'Duplicated Code' })
+            }
+        }
+
+        console.log(error)
+
+        return reply.status(500).send({ message: 'Internal Error'})
+    }
 })
 
 app.listen({
-    port: 3000,
+    port: portNumber,
 }).then(() => {
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port ', portNumber)
 })
